c4736376081002bc67a1706a5f669b37be8f751c
Fred Hu <65211410+fredhu0514@users.noreply.github.com>
2020-12-06 00:26:29 +0800

    Hide Instant

---
+++ WannaHideApps/WannaHideApps/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WannaHideApps
+//
+//  Created by Fred Hu on 2020/12/6.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ WannaHideApps/WannaHideApps/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  WannaHideApps
+//
+//  Created by Fred Hu on 2020/12/6.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
